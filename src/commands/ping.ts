6cc23e51b2bef4219ec91b78a519f3653d43140b
Leamir <16721818+leamir@users.noreply.github.com>
2024-05-30 19:08:29 -0300

    Update ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,20 @@
-import { CommandInteraction, Interaction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { commandFileClass } from "../helpers/fileClasses";
+import { client } from "../util";
 
 export default new commandFileClass(
     new SlashCommandBuilder()
         .setName('ping')
-        .setDescription("Pong"),
+        .setDescription("Retorna o valor "),
     (interaction: CommandInteraction) => {
-        interaction.reply('Pong!');
+        const responseEmbed = new EmbedBuilder()
+            .setColor(0x00FF00)
+            .setTitle(':ping_pong: Pong!')
+            .addFields(
+                { name: 'Tempo de resposta da API Discord', value: `${Math.abs(client.ws.ping)}ms`},
+                { name: 'Tempo de recebimento da mensagem', value: `${Math.abs(Date.now() - interaction.createdTimestamp)}ms`}
+            )
+        interaction.reply({ content: '', embeds: [responseEmbed], ephemeral: true});
     },
     () => {
 
